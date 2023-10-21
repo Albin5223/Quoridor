570f1a409096c31cc9e6a024a2342d1a049948ca
JOFFRIN Evan <evan.joffrin@interexa.fr>
2023-10-21 20:59:34 +0200

    Fix .gitignore; modification types

--- quoridor/lib/Types.mli
+++ quoridor/lib/Types.mli
@@ -1,11 +1,24 @@
-type  color = Black | White
+(* Spécification des différents types du jeu (joueurs, plateau, murs, ...) *)
+
+type color = Black | White
+
 type position = XY of int * int
+
 type player = {
   mutable position : position;
   color : color
 }
 
 type wall = position * position
-type board = position list
+
+type board = position list list
 
 type state = Ingame | EndGame
+
+type game = {
+  players: player list;
+  walls: wall list;
+  board: board;
+  current_player: player;
+  game_state: state;
+}
