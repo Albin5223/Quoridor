6f16a1cc70aef22893cfa07d6ac484f4719f6809
ELJAMAI Ali <alieljamai92@gmail.com>
2023-10-22 13:00:59 +0200

    fixing bugs, errors and inconsistent code

--- quoridor/lib/board.ml
+++ quoridor/lib/board.ml
@@ -3,46 +3,43 @@ open Types
 
 let boardSize = 17
 
-let initBoard () = 
-  let row = List.init boardSize (fun _ -> None) in
+let board = 
+  let row = List.init boardSize (fun _ -> Empty) in
   List.init boardSize (fun _ -> row)
 
-let getCellContent board (x, y) = 
+  
+let getCellContent pos = let (x, y) = pos in
   try 
     match List.nth (List.nth board y) x with
-    | Some content -> content
-    | None -> raise (OutOfBounds "Invalid cell content")
-  with Failure _ -> raise (OutOfBounds "Position is outside the board boundaries")  
-
-let getCoordinates = function 
-| XY (x, y) -> 
-  if x >= 0 && x < boardSize && y >= 0 && y < boardSize then (x, y)
-  else raise (OutOfBounds "Position is outside the board boundaries")
-
-let getX position = fst (getCoordinates position)
-let getY position = snd (getCoordinates position)
-
-let isValidPosition (x, y) =
+    |content -> content
+  with Failure _ -> raise (OutOfBounds "Position is outside the board boundaries") 
+  
+let isValidPosition pos = let (x, y) = pos in
   if x >= 0 && x < boardSize && y >= 0 && y < boardSize then true else false
+  
+let isWall = function 
+  |Wall -> true
+  |_ -> false   
 
-let isWallPosition (x, y) =
-  (x mod 2 = 0 || y mod 2 = 0) && (x mod 2 <> y mod 2)
+let isWallPosition pos = let (x, y) = pos in
+  isWall (getCellContent (x,y))
+    
 
-let isWallAdjacent position walls =
-  let (x, y) = getCoordinates position in
+
+let getX position = fst position
+let getY position = snd position
+
+ 
+  
+let isWallAdjacent pos =
+  let (x, y) = pos in
   
     if not (isValidPosition (x, y)) then raise (OutOfBounds "Position is outside the board boundaries");
     if isWallPosition (x, y) then raise (InvalidWallPosition "Given position is a wall position");
 
-    (* Helper function to determine if a position is part of a wall *)
-    let isPartOfWall pos wall =
-      let (wall_start, wall_end) = wall in
-        pos = getCoordinates wall_start || pos = getCoordinates wall_end
-      in
-
         (* Check if there's a wall at the specific position *)
         let checkWallAtPosition (dx, dy) =
-          isValidPosition (x + dx, y + dy) && List.exists (fun wall -> isPartOfWall (x + dx, y + dy) wall) walls
+          isValidPosition (x + dx, y + dy) && isWall (getCellContent (x + dx, y + dy))
         in
 
           (* Check all surrounding positions *)
@@ -51,7 +48,7 @@ let isWallAdjacent position walls =
   
 
 (*
-let lstOfMoves pl1 pl2 = 
+let lstOfMoves pl1 = 
   definir selon si il y a a coté un/une:
   - mur
   -extremite
