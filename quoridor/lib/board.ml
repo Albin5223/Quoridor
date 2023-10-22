d16f527fa88facead01c6548dfde7be4875c57d5
ELJAMAI Ali <alieljamai92@gmail.com>
2023-10-22 19:40:30 +0200

    start of adding movement functions and adding comments

--- quoridor/lib/board.ml
+++ quoridor/lib/board.ml
@@ -1,27 +1,32 @@
-(* Spécification de la structure du plateau de jeu et de ses caractéristiques; fonctions liées à la vérification des déplacements, à l'affichage... *)
+(*Specification of the structure of the game board and its characteristics; functions related to movement verification, display, etc.*)
 open Types
 
-let boardSize = 17
+let boardSize = 17 (*size of the board in length and width*)
+let wallStart = 10 (*Number of walls per player at the start of the game*)
 
-let board = 
+let board =  (*board initialization*)
   let row = List.init boardSize (fun _ -> Empty) in
   List.init boardSize (fun _ -> row)
 
+
+let playerTest = {position = (0,0); walls_left = wallStart; color = White}  (*Initializing a test player (to be removed later)*)
+
+let game = { players = []; board = board; current_player = playerTest; game_state = Ingame} (*initialization of the game (to be modified later)*)
   
-let getCellContent pos = let (x, y) = pos in
+let getCellContent pos = let (x, y) = pos in (* Returns the contents of a cell*)
   try 
     match List.nth (List.nth board y) x with
     |content -> content
   with Failure _ -> raise (OutOfBounds "Position is outside the board boundaries") 
   
-let isValidPosition pos = let (x, y) = pos in
+let isValidPosition pos = let (x, y) = pos in (*Tests if the given position is valid*)
   if x >= 0 && x < boardSize && y >= 0 && y < boardSize then true else false
   
-let isWall = function 
+let isWall = function  (*Test if the cell contains a wall*)
   |Wall -> true
   |_ -> false   
 
-let isWallPosition pos = let (x, y) = pos in
+let isWallPosition pos = let (x, y) = pos in (*Tests if the given position can contain a wall*)
   isWall (getCellContent (x,y))
     
 
@@ -31,7 +36,7 @@ let getY position = snd position
 
  
   
-let isWallAdjacent pos =
+let lstOfMoves_wall pos = (*Returns a list of valid movements according to the positions of the walls around*)
   let (x, y) = pos in
   
     if not (isValidPosition (x, y)) then raise (OutOfBounds "Position is outside the board boundaries");
@@ -39,19 +44,21 @@ let isWallAdjacent pos =
 
         (* Check if there's a wall at the specific position *)
         let checkWallAtPosition (dx, dy) =
-          isValidPosition (x + dx, y + dy) && isWall (getCellContent (x + dx, y + dy))
+          if isValidPosition (x + dx, y + dy)  then 
+            if isWall (getCellContent (x + dx, y + dy)) then [(x + dx, y + dy)] else []
+          else raise (OutOfBounds "Position is outside the board boundaries")
         in
 
           (* Check all surrounding positions *)
-          checkWallAtPosition (-1, 0) || checkWallAtPosition (1, 0) || 
-          checkWallAtPosition (0, -1) || checkWallAtPosition (0, 1)
+          checkWallAtPosition (-1, 0) @ checkWallAtPosition (1, 0) @ 
+          checkWallAtPosition (0, -1) @ checkWallAtPosition (0, 1)
   
 
-(*
-let lstOfMoves pl1 = 
-  definir selon si il y a a coté un/une:
-  - mur
-  -extremite
-  -joueur
-  -rien
-  *)
+
+
+let lstOfMoves pos =  (*returns the list of possible moves depending on the position of a player*)
+  let lst = lstOfMoves_wall pos in 
+    let rec checkPlayerPos players lst = match players with
+      |pl :: l -> let lst = List.filter (fun (a,b) -> a <> (getX (pl.position)) && b <> (getY (pl.position))) lst in checkPlayerPos l lst 
+      |_ -> lst
+    in checkPlayerPos game.players lst
