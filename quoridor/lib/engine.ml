ef235300c26a6c7f750b49b8c3b1e386254aa263
Ali EL JAMAI <alieljamai92@gmail.com>
2023-10-26 21:29:16 +0200

    changing function names to use snake case

--- quoridor/lib/engine.ml
+++ quoridor/lib/engine.ml
@@ -2,16 +2,16 @@
 open Board
 open Types
 
-let move game player = let lstMv = listOfMoves (player.position) in
+let move game player = let lstMv = list_of_moves (player.position) in
     let r = Random.int (List.length lstMv) in let newPos = List.nth lstMv r in 
-    changePosOfPlayer game newPos
+    change_pos_of_player game newPos
       
 
-let placeWall game player = let r = Random.int 2 in
-if (r == 0) then (placeVerticalWall game player.position)
-else (placeHorizontalWall game player.position)
+let place_wall game player = let r = Random.int 2 in
+if (r == 0) then (place_vertical_wall game player.position)
+else (place_horizontal_wall game player.position)
 
-let detMove game player =
+let det_move game player =
   let r = Random.int 2 in 
     if (r == 0) then (move game player)
-    else placeWall game player
+    else place_wall game player
