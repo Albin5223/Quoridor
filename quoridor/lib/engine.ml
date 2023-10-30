ffbf2ad4dd74144d1c3e2cb11d57f88b148e7e86
JOFFRIN Evan <evan.joffrin@interexa.fr>
2023-10-30 12:05:42 +0100

    Fixing conflits at board and engine; needs to review engine.

--- quoridor/lib/engine.ml
+++ quoridor/lib/engine.ml
@@ -1,17 +1,40 @@
-(* Module contenant la logique du jeu, la victoire, les mouvements, ... *)
 open Board
 open Types
 
-let move game player = let lstMv = list_of_moves (player.position) in
-    let r = Random.int (List.length lstMv) in let newPos = List.nth lstMv r in 
-    change_pos_of_player game newPos
-      
+let change_pos_of_player game pos =
+  let newBoard = Array.map Array.copy game.board in
+  let player = game.current_player in 
+  let (x, y) = pos in
+  newBoard.(y).(x) <- Player player;
+  let (x_old, y_old) = player.position in 
+  newBoard.(y_old).(x_old) <- Empty;
+  { game with board = newBoard; current_player = { game.current_player with position = pos } }
 
-let place_wall game player = let r = Random.int 2 in
-if (r == 0) then (place_vertical_wall game player.position)
-else (place_horizontal_wall game player.position)
+let move game player = 
+    let lstMv = list_of_moves player.position game.board in
+    match lstMv with
+    | [||] -> game
+    | _ -> 
+        let r = Random.int (Array.length lstMv) in 
+        let newPos = lstMv.(r) in 
+        change_pos_of_player game newPos
+
+let rec place_wall_random game player =
+  let rec generate_random_wall_pos () =
+      let x = Random.int board_size in
+      let y = Random.int board_size in
+      if x mod 2 = 1 && y mod 2 = 1 && is_wall_position (x, y) then (x, y)
+      else generate_random_wall_pos ()
+  in
+  let wall_pos = generate_random_wall_pos () in
+  try 
+      { game with board = place_wall wall_pos [|player.position|] game.board }
+  with InvalidWallPlacement _ -> 
+      place_wall_random game player
 
 let det_move game player =
-  let r = Random.int 2 in 
-    if (r == 0) then (move game player)
-    else place_wall game player
+    let r = Random.int 2 in 
+    if r == 0 then move game player
+    else place_wall_random game player
+
+(* TODO : verify that code is running correctly, add robustness and complets it *)
--- quoridor/lib/types.mli
+++ quoridor/lib/types.mli
@@ -10,23 +10,9 @@ type game = {
   current_player : player;
   game_state : state;
 }
-
-type cell_content =
-  | Empty
-  | Wall
-  | Player of player
-
-type board = cell_content array array  (* Game board *)
-
-type state = Ingame | EndGame of player  (* Game's state: ongoing or finished with a winner *)
-
-type game = {  (* Game's attributes *)
-  players: player list;          (* List of players *)
-  board: board;                  (* Game board *)
-  current_player: player;        (* Player whose turn it is *)
-  game_state: state;             (* Current game state *)
-}
-
-exception OutOfBounds of string           (* Raised when a position is outside the board *)
-exception InvalidWallPosition of string   (* Raised for walls in invalid positions *)
-exception InvalidMove of string           (* Raised when a move is illegal, i.e. out of bounds, on other player / wall... *)
+exception OutOfBounds of string
+exception InvalidWallPosition of string
+exception InvalidPlayerPosition of string
+exception InvalidMove of string
+exception InvalidPosition of string
+exception InvalidWallPlacement of string
