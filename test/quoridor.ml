63cf6a76df2b497db1fa11ba717ce0a7d3c2b6d0
ABDOROHMANG Mathaui Yves <mathauiabdorohmang@gmail.com>
2023-11-14 13:17:22 +0100

    fix on returns_exception function

--- test/quoridor.ml
+++ test/quoridor.ml
@@ -1,15 +1,11 @@
 open Quoridor
-(* let reference_game = Engine.init_game 4 *)
 
 let returns_exception f x =
   try 
-    let _ = f x in true
+    ignore (f x); false
   with
-    | _ -> false
+    | _ -> true
 
-
-(* let random_nb_players = 
-  QCheck.(--) 2 4 *)
 
 let qcheck_test name objects f =
   QCheck.Test.make ~count:1000
@@ -31,21 +27,10 @@ let qch_to_alc_uniq test =
   qch_to_alc [test]
 
 let test_players = 
-  let ppp = QCheck.(--) 0 1 in
-  qcheck_test "For all n, init_game n when n in [|0,1|], returns an exception" 
-  ppp (fun n -> returns_exception Engine.init_game n)
+  let zero_or_one = QCheck.(--) 5 50 in
+  qcheck_test "For all n, init_game n when n in [|5,50|], returns an exception" 
+  zero_or_one (fun n -> returns_exception Engine.init_game n)
     
-  
-  (*
-  let passing =
-    QCheck.Test.make ~count:1000
-      ~name:"list_rev_is_involutive"
-      QCheck.(list small_int)
-      (fun l -> List.rev (List.rev l) = l);;
-
-let suite =
-  List.map QCheck_alcotest.to_alcotest [passing]
-  *)
 
 let () =
   let open Alcotest in
