c2a01cf01b465308e75f6538d4122c4ec337864c
ELJAMAI Ali <alieljamai92@gmail.com>
2023-11-14 21:36:10 +0100

    initialization of the game with our random strategy

--- bin/main.ml
+++ bin/main.ml
@@ -1,40 +1,51 @@
 open Quoridor.Engine
+open Quoridor.Board
+open Quoridor.Types
 
-
-let random_move () =
-  let lstMv = Board.list_of_moves (pos_current_player ()) in
+let random_move pos =
+  let lstMv = list_of_moves pos in
   match lstMv with
-  | [] -> ()
+  | [] -> raise (NoMove "There is no movement possible for this player")
   | _ ->
       let r = Random.int (List.length lstMv) in
       let newPos = List.nth lstMv r in
-      Board.move_player newPos
+        Moving newPos
 
-let rec place_wall_random () =
+let  pos_wall_random  =
   let rec generate_random_wall_pos () =
-    let x1 = Random.int Board.board_size in
-    let y1 = Random.int Board.board_size in
+    let x1 = Random.int board_size in
+    let y1 = Random.int board_size in
     let r = Random.int 4 in
-    let xv, yv = List.nth Board.move_vectors r in
-    Format.printf "%d,%d " x1 y1;
+    let xv, yv = List.nth move_vectors r in
+    Format.printf "pos wall :%d,%d " x1 y1;
     try
       if
-        Board.is_wall_position (x1, y1)
-        && Board.is_wall_position (x1 + xv, y1 + yv)
+        is_wall_position (x1, y1)
+        && is_wall_position (x1 + xv, y1 + yv)
       then ((x1, y1), (x1 + xv, y1 + yv))
       else generate_random_wall_pos ()
     with InvalidPosition _ -> generate_random_wall_pos ()
   in
   let wall_pos1, wall_pos2 = generate_random_wall_pos () in
-    try Board.place_wall wall_pos1 wall_pos2
-    with InvalidWallPlacement _ -> place_wall_random ()
+    Wall (wall_pos1, wall_pos2)
 
-let det_move () =
+let det_move pos =
   let r = Random.int 3 in
-  if r == 0 && walls_left_current_player () > 0 then place_wall_random
-  else random_move
+  if r == 0 && walls_left_current_player () > 0 then pos_wall_random
+  else random_move pos
+
+let create_lst_of_attributs () =
+  let colors = [Red; Blue; Green; Yellow] in
+    let positions = [
+      (board_size / 2, 0);
+      (board_size / 2, board_size - 1);
+      (0, board_size / 2);
+      (board_size - 1, board_size / 2);] 
+    in
+      List.init 4 (fun i -> ((List.nth colors i), (List.nth positions i), det_move))
 
 
-let () =
-  let _ = run_game () in
+
+let () = let lst_attribut = create_lst_of_attributs () in
+  let _ = run_game lst_attribut in
   ()
