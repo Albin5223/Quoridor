ab3e07e5add9d887b73999bb1df57ec200ef3114
ABDOROHMANG Mathaui Yves <mathauiabdorohmang@gmail.com>
2023-11-14 14:28:08 +0100

    added more tests + restructuration

--- test/quoridor.ml
+++ test/quoridor.ml
@@ -1,8 +1,16 @@
 open Quoridor
 
+(* AUXILIARY FUNCTIONS *)
+
 let returns_exception f x =
   try 
     ignore (f x); false
+  with
+    | _ -> true
+
+let returns_exception_unit f = 
+  try
+    ignore f; false
   with
     | _ -> true
 
@@ -12,6 +20,14 @@ let qcheck_test name objects f =
   ~name: name
   objects f
 
+let qch_to_alc tests_list =
+  List.map QCheck_alcotest.to_alcotest tests_list
+    
+let qch_to_alc_uniq test =
+  qch_to_alc [test]
+
+(* TESTS *)
+
 let test_position =
   let random_correct_pos =
     let x = QCheck.(--) 0 16 in
@@ -20,21 +36,29 @@ let test_position =
   in 
   qcheck_test "For all positions (x,y) such that x,y in [|0,16|]², (x,y) is a valid position " random_correct_pos (fun pair -> Board.is_valid_position pair)
 
-let qch_to_alc tests_list =
-  List.map QCheck_alcotest.to_alcotest tests_list
-
-let qch_to_alc_uniq test =
-  qch_to_alc [test]
-
 let test_players = 
   let zero_or_one = QCheck.(--) 5 50 in
   qcheck_test "For all n, init_game n when n in [|5,50|], returns an exception" 
   zero_or_one (fun n -> returns_exception Engine.init_game n)
-    
+
+let test_players_2 () =
+  Alcotest.(check bool) "same bool" true (returns_exception Engine.init_game 0)
+
+let test_players_3 () =
+  Alcotest.(check bool) "same bool" true (returns_exception Engine.init_game 1)
+
+
+
+let test_finite_game = 
+  qcheck_test "Game always ends" (QCheck.int)
+  (fun _ -> not (returns_exception_unit Engine.run_game))
+   
 
 let () =
   let open Alcotest in
-  run "Quoridor tests" [
-    "correct_positions" , qch_to_alc_uniq test_position;
-    "incorrect_nb_of_players" , qch_to_alc_uniq test_players;
-  ];
+    run "Quoridor tests" [
+      "correct_positions" , qch_to_alc_uniq test_position;
+      "incorrect_nb_of_players" , qch_to_alc_uniq test_players @ 
+        [test_case "0 player" `Quick test_players_2; test_case "1 player" `Quick test_players_3];
+      "correct_game", qch_to_alc_uniq test_finite_game 
+    ];
