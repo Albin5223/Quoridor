b72be66070af3a14f09e11d6235aa43dbda352ec
IGLESIAS VAZQUEZ yago <yago.iglesias-vazquez@etu.u-paris.fr>
2023-11-23 15:31:49 +0100

    bug: walls can be removed
    As we have access to `reset_board` we can just remove wall and
    move players to their initial positions (or where we want).

--- test/test_engine.ml
+++ test/test_engine.ml
@@ -1,76 +1,52 @@
-(*open Quoridor
-  open Quoridor.Types
+open Quoridor.Types
+open Quoridor.Board
+open Quoridor.Engine
 
-  module Strategy = struct
-    open Quoridor.Types
-    open Quoridor.Board
+let init_game player_info =
+  player_info
+  |> List.map (fun (color, pos, strat) ->
+         {
+           color;
+           start_position = pos;
+           current_position = pos;
+           strategy = strat;
+           walls_left = 10;
+         })
+  |> List.iter add_player_to_board;
+  start_game ()
 
-    let random_move pos =
-      let lstMv = list_of_moves pos in
-      match lstMv with
-      | [] -> raise (NoMove "There is no movement possible for this player")
-      | _ ->
-          let r = Random.int (List.length lstMv) in
-          let newPos = List.nth lstMv r in
-          Moving newPos
+let get_all_walls () =
+  List.init board_size (fun x -> x)
+  |> List.map (fun x -> List.init board_size (fun y -> (x, y)))
+  |> List.flatten |> List.filter is_wall
 
-    let pos_wall_random () =
-      let rec generate_random_wall_pos () =
-        let x1 = Random.int board_size in
-        let y1 = Random.int board_size in
-        let r = Random.int 4 in
-        let xv, yv = List.nth move_vectors r in
-        try
-          validate_wall_placement
-            (current_player ())
-            (x1, y1)
-            (x1 + xv, y1 + yv);
-          ((x1, y1), (x1 + xv, y1 + yv))
-        with
-        | InvalidWallPosition _ -> generate_random_wall_pos ()
-        | InvalidPosition _ -> generate_random_wall_pos ()
-        | InvalidWallPlacement _ -> generate_random_wall_pos ()
-      in
-      let wall_pos1, wall_pos2 = generate_random_wall_pos () in
-      Wall (wall_pos1, wall_pos2)
+let test_walls_cannot_be_removed =
+  let players strat1 strat2 =
+    [ (Red, (0, board_size / 2), strat1); (Blue, (board_size / 2, 0), strat2) ]
+  in
+  let wall_placer _ =
+    let wall_pos1 = (1, 0) in
+    let wall_pos2 = (1, 1) in
+    Placing_wall (wall_pos1, wall_pos2)
+  in
+  let rec reseter_strat _ =
+    reset_board ();
+    players wall_placer reseter_strat |> init_game;
+    (* This should be the actual test :
+       `first_pick_strategy pos`
+       but since the current player has been updated, `pos` means nothing, as it is
+       the position of the player who just played. So I joust hardcoded a position that
+       t should work is walls are immutable but current player changes thanks to `reset_board`*)
+    Moving (2, board_size / 2)
+  in
 
-    let det_move pos =
-      let r = Random.int 3 in
-      if r == 0 && (current_player ()).walls_left > 0 then pos_wall_random ()
-      else random_move pos
-  end
+  Alcotest.test_case "Walls cannot be removed" `Quick (fun () ->
+      reset_board ();
+      players reseter_strat wall_placer |> init_game;
+      play ();
+      play ();
+      Alcotest.(check int) "Number of walls" 2 (get_all_walls () |> List.length))
 
-  let test_init_game =
-    Alcotest.test_case "init_game" `Quick (fun () ->
-        Board.reset_board ();
-        let player_attributes =
-          [
-            (Red, (0, Board.board_size / 2), Strategy.det_move);
-            (Blue, (Board.board_size / 2, 0), Strategy.det_move);
-          ]
-        in
-        Engine.init_game player_attributes;
-        let is_red_player = Board.is_player (0, Board.board_size / 2) in
-        let is_blue_player = Board.is_player (Board.board_size / 2, 0) in
-        Alcotest.(check bool) "Red player added" true is_red_player;
-        Alcotest.(check bool) "Blue player added" true is_blue_player)
-
-  let test_play =
-    Alcotest.test_case "play" `Quick (fun () ->
-        Board.reset_board ();
-        let player_attributes =
-          [
-            (Red, (0, Board.board_size / 2), Strategy.det_move);
-            (Blue, (Board.board_size / 2, 0), Strategy.det_move);
-          ]
-        in
-        Engine.init_game player_attributes;
-        Board.start_game ();
-        Engine.play ();
-        Alcotest.(check unit) "Player played a turn" () ())
-
-  let () =
-    let open Alcotest in
-    run "Engine Tests"
-      [ ("init_game", [ test_init_game ]); ("play", [ test_play ]) ]
-*)
+let () =
+  let open Alcotest in
+  run "Engine" [ ("Walls cannot be removed", [ test_walls_cannot_be_removed ]) ]
