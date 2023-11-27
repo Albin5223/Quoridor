4d25552397cb068f1dd87ae471f43d653c976244
eljamai <alieljamai92@gmail.com>
2023-11-27 23:05:57 +0100

    adding test

--- lib/board.mli
+++ lib/board.mli
@@ -76,7 +76,7 @@ val list_of_moves : Types.position -> Types.position list
 val validate_wall_placement :
   Types.player -> Types.position -> Types.position -> unit
 (** Validates the placement of a wall by a player.
-    @param wallsLeft The number of walls the player has left.
+    @param player The player placing the wall.
     @param pos1 The first position of the wall.
     @param pos2 The second position of the wall.
     @raise InvalidWallPlacement if the player has no walls left or the wall placement is invalid.
